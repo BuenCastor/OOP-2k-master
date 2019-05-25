66957ee81353e6bdf33febf977a64866d8c7cb4e
sauchkin967 <sauchkin967@gmail.com>
2019-05-25 16:27:22 +0300

    update

---
+++ src/main/java/ua/edu/chmnu/fks/oop/lab_5/Main.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ua.edu.chmnu.fks.oop.lab_5;
+import ua.edu.chmnu.fks.oop.lab_5.Triangle;
+/**
+ *
+ * @author ARTEM
+ */
+public class Main {
+    public static void main(String[] args){
+    Triangle triangle = new Triangle();
+    triangle.drawShape();
+    
+    }
+}
